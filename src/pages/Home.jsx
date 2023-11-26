1a59948de641c5b776ae85791b631c5726e975c2
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-11-26 16:11:05 +0530

    Add. Included the Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Row, Col, Button} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import './Home.css'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return <Row>
+        <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
+            <div>
+                <h1>Share the world with your friends</h1>
+                <p>Chat App lets you connect with the world</p>
+                <LinkContainer to="/chat">
+                    <Button variant='success'>Get Started <i className='fas fa-comment home-message-icon'></i></Button>
+                   
+                </LinkContainer>
+            </div>
+        </Col>
+        <Col md={5} className='home__bg'>
+        
+        </Col>
+    </Row>
 }
 
 export default Home
