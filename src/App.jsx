bd59914c510ee742b6057cd1afe04ea34901a88f
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-11-26 16:05:00 +0530

    Add. Included the Basic Navigation Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import NavigationComponent from './components/NavigationComponent'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Chat from './pages/Chat'
 
 
 
 const App = () => {
     return (
-        <h1>Chat App </h1>
+        <Router>
+            <NavigationComponent/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/signup' element={<Signup/>}/>
+                <Route path='/chat' element={<Chat/>}/>
+            </Routes>
+        </Router>
     )
 }
 
